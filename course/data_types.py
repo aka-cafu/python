c6c9154a694711404906ee5731addbd973a8a665
Felipe Gomes <felipe.gomes@vr.com.br>
2023-05-23 17:46:53 -0300

    (Added) Aula de string e inteiros

---
+++ course/data_types.py
@@ -0,0 +1,6 @@
+print('python => string') # string
+print(2+1, '=> int') # int / inteiro 
+print(2.1, '=> float') # float
+print(["banana", "laranja", "uva"], '=> lista') # lista / list
+print({"nome":"Felipe"}, '=> dicionarion') # dicionario
+print((10, "nome", 2.1), '=> tupla') # tuple / tupla
